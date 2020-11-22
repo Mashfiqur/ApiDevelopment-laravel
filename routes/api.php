970e9fe9c7bbe901a17a26f7c08a7cccb5bd8c80
mashfiqur <mashfiqurrr@gmail.com>
2020-11-21 23:17:23 -0800

    get,show,create,destroy,update

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Post;
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,9 +18,49 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/test-the-api',function()
-{
+// Route::get('/test-the-api',function()
+// {
+// return ['message' => 'Hello!'];
+// });
 
-return ['message' => 'Hello!'];
+//posts-CRUD-(http request)
+// get all(GET)/api/posts
+//create a single(POST)/api/posts
+// get a single row(GET)/api/posts/{id}
+// update a single(PUT/PATCH)/api/posts/{id}
+// delete (DELETE)/api/posts/{id}
 
-});
+//To create a resource in Laravel
+// 1.Create the database and migrations
+// 2. create a model
+// 3.create a service : Eloquent ORM
+// 4.create a controller to go,get info from the database.
+// 5.Return that info
+
+// Route::get('/posts',function(){
+
+//     $post = Post::create([
+//         'title' => 'my first post',
+//         'slug' => 'my first slug'
+//     ]);
+//     return $post;
+// });
+
+// //show all mode
+Route::get('/posts',[PostController::class, 'index']);
+
+
+// //show single mode
+Route::get('/posts/{id}',[PostController::class, 'show']);
+
+//store/create mode
+Route::post('/posts',[PostController::class, 'store']);
+
+//update mode
+Route::put('/posts/{id}',[PostController::class, 'update']);
+
+//delete mode
+Route::delete('/posts/{id}',[PostController::class, 'destroy']);
+
+
+// Route::resource('posts',[PostController::class]);
